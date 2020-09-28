ae50ef6016223a53dcea643c1b18d3d4539136ef
abdo <amahany@arabia-it.net>
2020-09-28 20:16:06 +0200

    update model class

--- Domain/src/main/java/com/stockcontrol/domain/model/StockItem.kt
+++ Domain/src/main/java/com/stockcontrol/domain/model/StockItem.kt
@@ -3,3 +3,12 @@ package com.stockcontrol.domain.model
 data class StockItem( val id : String , val name : String , val price : String
                       , val stock: List<Stock> , val sales : List<Sales>) {
 }
+
+
+data class Sales(val invoiceNo : String , val invoiceDate : String , val client : String
+                 , val invoiceQty : String , val reservedQty : String) {
+}
+
+
+data class Stock( val id : String , val availableQty : String , val reservedQty : String) {
+}
